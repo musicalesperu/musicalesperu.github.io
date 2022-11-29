ecc4814d884ffe540915937d548f9daee193d382
musicalesperu <musicalesperu@gmail.com>
2022-11-29 20:13:14 +0000

    291122

---
+++ Assets/scripts/nav.js
@@ -0,0 +1,163 @@
+document.write(`
+
+<ul>
+
+<li><a class="active3" href="contact.html">&nbsp;&nbsp;&nbsp;<img src="images/misc/mail.png">&nbsp;&nbsp;&nbsp;</a>
+<li><a class="active" href="guide.html">Guía de intercambio<br>Trade Guidelines</a>
+<li><a class="active" href="notes.html">Notas sobre las listas<br>Notes On The Listings</a>
+<li class="active"><a href="links.html">Enlaces&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>Links&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+<li class="active"><a href="wants.html">Lista de Deseos<br>Wants&nbsp;List</a><li>
+
+<li class="dropdown">
+<a href="javascript:void(0)" class="dropbtn">Libros&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>Books&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+<div class="dropdown-content">
+<a href="ebooks.html">Libros electronicos<br>eBooks</a>
+<a href="audiobooks.html">Audiolibros<br>Audiobooks</a>
+</div>
+
+<li class="dropdown"></a>
+<a href="javascript:void(0)" class="dropbtn">Partituras y libretos<br>Scores &amp; Libretti</a>
+<div class="dropdown-content">
+<a href="scores.html">Partituras Musicales<br>Musical Scores</a>
+<a href="libretti.html">Libretos Muscales<br>Musical Libretti</a>
+<a href="scores2.html">Partituras Opéra<br>Opera Scores</a>
+<a href="libretti2.html">Libretos Opéra<br>Opera Libretti</a>
+</div>
+
+<li class="dropdown">
+<a href="javascript:void(0)" class="dropbtn">Otros Vídeos&nbsp;&nbsp;&nbsp;&nbsp;<br>Other Videos&nbsp;&nbsp;&nbsp;&nbsp;</a>
+<div class="dropdown-content">
+<a href="musicvid.html">Vídeos de Música<br>Music Videos</a>
+<a href="movies.html">Películas<br>Movies</a>
+<a href="lgbt.html">Pelicúlas de temática gay<br>Gay-themed Movies</a>     
+<a href="tvshows.html">Telévision<br>TV Shows</a>
+<a href="docs2.html">Documentales de largometraje<br>Feature Documentaries</a>
+</div>
+
+
+<li class="dropdown">
+<a href="javascript:void(0)" class="dropbtn">Otro Audio&nbsp;&nbsp;&nbsp;&nbsp;<br>Other Audio&nbsp;&nbsp;&nbsp;&nbsp;</a>
+<div class="dropdown-content">
+<a href="vocalists3.html">Vocalistas<br>Vocalists</a>
+<a href="compilations2.html">Compilaciónes<br>Compilations</a>
+<a href="latina.html">Música Latina<br>Latin Music</a>
+<a href="francaise.html">Música Frances<br>French Music</a>
+<a href="comedy.html">Comedia<br>Comedy</a>
+<a href="christmas.html">Música Navidad<br>Christmas Music</a>
+</div>
+
+<li class="dropdown">
+<a href="javascript:void(0)" class="dropbtn">Música Clasica<br>Classical Music</a>
+<div class="dropdown-content">
+<a href="opera.html">Opéra<br>Opera</a>
+<a href="operetta.html">Opereta<br>Operetta</a>
+<a href="classical.html">Música Clasica<br>Classical Music</a>
+<a href="vocalists2.html">Vocalistas<br>Vocalists</a>
+<a href="operavid.html">Vídoes de Opéra<br>Opera Videos</a>
+</div>
+
+<li class="dropdown">
+<a href="javascript:void(0)" class="dropbtn">Vídeos de Teatro<br>Theatre Videos</a>
+<div class="dropdown-content">
+<a href="musicals.html">Vídeos comerciales<br>Commercial Videos</a>
+<a href="bootleg.html">Vídeos no comerciales<br>Non-Commercial Videos</a>
+<a href="concerts.html">Concerts<br>Conciertos</a>
+<a href="docs.html">Documentos<br>Documentaries</a>
+<a href="playsvid.html">Obras de Teatro<br>Theatre Plays</a>
+</div>
+
+<li class="dropdown">
+<a href="javascript:void(0)" class="dropbtn">Relacionado de Teatro<br>Theatre Related</a>
+<div class="dropdown-content">
+<a href="vocalists.html">Vocalistas<br>Vocalists</a>
+<a href="compilations.html">Compilaciónes<br>Compilations</a>
+<a href="plays.html">Obras de Teatro<br>Theatre Plays</a>
+
+<li><a class="active" href="disney.html">Bandas Sonoras Disney<br>Disney Soundtracks</a></li>
+
+<li><a class="active" href="demo.html">Grabaciones Demo<br>Demo Recordings</a>
+
+
+<div class="subnav">
+<a href="javascript:void(0)" class="dropbtn">Bandas Sonoras<br>Cast Albums</a>
+<div class="subnav-content">
+<a href="joseph.html"><img src="images/misc/joseph.png" height="30px"></a>
+<a href="jcs.html"><img src="images/misc/jcs.png" height="30px"></a>
+<a href="evita.html"><img src="images/misc/evita.png" height="30px"></a>
+<a href="cats.html"><img src="images/misc/cats.png" height="30px"></a>
+<a href="starlight.html"><img src="images/misc/starlight.png" height="30px"></a>
+<a href="poto.html"><img src="images/misc/poto.png" height="30px"></a>
+<a href="sunset.html"><img src="images/misc/sunset.png" height="30px"></a>
+<a href="chess.html"><img src="images/misc/chess.png" height="30px"></a>
+<a href="lesmis.html"><img src="images/misc/lesmis.png" height="30px"></a>
+<a href="saigon.html"><img src="images/misc/saigon.png" height="30px"></a>
+<a href="tanz.html"><img src="images/misc/tanz.png" height="30px"></a>
+<a href="sisi.html"><img src="images/misc/sisi.png" height="30px"></a>
+<a href="sweeney.html"><img src="images/misc/sweeney.png" height="30px"></a>
+<a href="rocky.html"><img src="images/misc/rocky.png" height="30px"></a>
+<a href="fiddler.html"><img src="images/misc/fiddler.png" height="30px"></a>
+<a href="cabaret.html"><img src="images/misc/cabaret.png" height="25px"></a>
+<a href="chicago.html"><img src="images/misc/chicago.png" height="30px"></a>
+<a href="som.html"><img src="images/misc/som.png" height="30px"></a>
+<a href="mfl.html"><img src="images/misc/mfl.png" height="30px"></a>
+<a href="jekyll.html"><img src="images/misc/jekyll.png" height="30px"></a>
+<a href="mancha.html"><img src="images/misc/molm.png" height="30px"></a>
+<a href="west.html"><img src="images/misc/west2.png" height="30px"></a>
+<a href="hair.html"><img src="images/misc/hair.png" height="25px"></a>
+<a href="grease.html"><img src="images/misc/grease.png" height="30px"></a>
+<a href="Cast Albums 0.html">#</a>
+<a href="Cast Albums A.html">A</a>
+<a href="Cast Albums B.html">B</a>
+<a href="Cast Albums C.html">C</a>
+<a href="Cast Albums D.html">D</a>
+<a href="Cast Albums E.html">E</a>
+<a href="Cast Albums F.html">F</a>
+<a href="Cast Albums G.html">G</a>
+<a href="Cast Albums H.html">H</a>
+<a href="Cast Albums I.html">I</a>
+<a href="Cast Albums J.html">J</a>
+<a href="Cast Albums K.html">K</a>
+<a href="Cast Albums L.html">L</a>
+<a href="Cast Albums M.html">M</a>
+<a href="Cast Albums N.html">N</a>
+<a href="Cast Albums O.html">O</a>
+<a href="Cast Albums P.html">P</a>
+<a href="Cast Albums Q.html">Q</a>
+<a href="Cast Albums R.html">R</a>
+<a href="Cast Albums S.html">S</a>
+<a href="Cast Albums T.html">T</a>
+<a href="Cast Albums U.html">U</a>
+<a href="Cast Albums V.html">V</a>
+<a href="Cast Albums W.html">W</a>
+<a href="Cast Albums XYZ.html">XY Z</a>
+</div>
+</div>
+
+<div class="subnav"><a href="javascript:void(0)" class="dropbtn">Audios no comerciales<br>Non-Commercial Audios</a>
+<div class="subnav-content">
+<a href="josephboots.html"><img src="images/misc/joseph.png" height="30px"></a>
+<a href="jcsboots.html"><img src="images/misc/jcs.png" height="30px"></a>
+<a href="evitaboots.html"><img src="images/misc/evita.png" height="30px"></a>
+<a href="catsboots.html"><img src="images/misc/cats.png" height="30px"></a>
+<a href="starlightboots.html"><img src="images/misc/starlight.png" height="30px"></a>
+<a href="potoboots.html"><img src="images/misc/poto.png" height="30px"></a>
+<a href="sunsetboots.html"><img src="images/misc/sunset.png" height="30px"></a>
+<a href="chessboots.html"><img src="images/misc/chess.png" height="30px"></a>
+<a href="lesmisboots.html"><img src="images/misc/lesmis.png" height="30px"></a>
+<a href="saigonboots.html"><img src="images/misc/saigon.png" height="30px"></a>
+<a href="tanzboots.html"><img src="images/misc/tanz.png" height="30px"></a>
+<a href="sisiboots.html"><img src="images/misc/sisi.png" height="30px"></a>
+<a href="sweeneyboots.html"><img src="images/misc/sweeney.png" height="30px"></a>
+<a href="rockyboots.html"><img src="images/misc/rocky.png" height="30px"></a>
+<a href="chicagoboots.html"><img src="images/misc/chicago.png" height="30px"></a>
+<a href="jekyllboots.html"><img src="images/misc/jekyll.png" height="30px"></a>
+<a href="boots1.html"># - E</a>
+<a href="boots2.html">F - K</a>
+<a href="boots3.html">L - Q</a>
+<a href="boots4.html">R - Z</a>
+</div>
+</div>
+
+<li><a class="active2" href="main.html">&nbsp;&nbsp;<img src="images/misc/home.png">&nbsp;&nbsp;</li></a>
+
+`);
