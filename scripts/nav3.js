e46f8f35f5b263fcf0ae02acf5013e2720496601
musicalesperu <musicalesperu@gmail.com>
2025-05-28 15:56:22 +0100

    New Layout

---
+++ scripts/nav3.js
@@ -0,0 +1,24 @@
+document.write(`
+
+<ul>
+
+<div>
+
+<li class="right"><a href="index.html"><img src="images/misc/home.png" height="30px"></li></a>
+
+<li><a class="active" href="vocalists3.html">Vocalistas & Grupos<br>Vocalists & Groups</a></li>
+<li><a class="active" href="latina.html">Música Latina<br>Latin Music</a></li>
+<li><a class="active" href="francaise.html">Música Frances<br>French Music</a></li>
+<li><a class="active" href="compilations2.html">Compilaciónes<br>Compilations</a></li>
+<li><a class="active" href="comedy.html">Comedia<br>Comedy</a></li>
+<li><a class="active" href="christmas.html">Música Navidad<br>Christmas Music</a></li>
+<li><a class="active" href="musicvid.html">Vídeos de Música<br>Music Videos</a></li>
+<li><a class="active" href="movies.html">Películas<br>Movies</a></li>
+<li><a class="active" href="lgbt.html">Películas de temática gay<br>Gay Themed Movies</a></li>
+<li><a class="active" href="docs2.html">Documentales de largometraje<br>Feature Documentaries</a></li>
+<li><a class="active" href="tvshows.html">Telévision<br>TV Shows</a></li>
+
+</div>
+</ul>
+
+`);
---
+++ scripts/nav4.js
@@ -0,0 +1,17 @@
+document.write(`
+
+<ul>
+
+<div>
+
+<li class="right"><a href="index.html"><img src="images/misc/home.png" height="30px"></li></a>
+<li><a class="active" href="scores.html">Partituras Musicales<br>Musical Scores</a></li>
+<li><a class="active" href="libretti.html">Libretos Muscales<br>Musical Libretti</a></li>
+<li><a class="active" href="scores2.html">Partituras Opéra<br>Opera Scores</a></li>
+<li><a class="active" href="libretti2.html">Libretos Opéra<br>Opera Libretti</a></li>
+<li><a class="active" href="ebooks.html">Libros electronicos<br>eBooks</a></li>
+<li><a class="active" href="audiobooks.html">Audiolibros<br>Audiobooks</a></li>
+</div>
+</ul>
+
+`);
